35737b6ecee554235a182cad2888e6481f0c5d40
awilson61 <144381426+awilson61@users.noreply.github.com>
2023-11-12 16:17:07 -0600

    Fixed the errors with strings in textbox and fixed a typo

--- logic.py
+++ logic.py
@@ -2,6 +2,7 @@ from PyQt6.QtWidgets import *
 from gui import *
 from datetime import date
 
+
 class Logic(QMainWindow, Ui_ProduceShop):
     def __init__(self):
         super().__init__()
@@ -9,7 +10,6 @@ class Logic(QMainWindow, Ui_ProduceShop):
         self.clear_button.clicked.connect(lambda: self.clear())
         self.submit_button.clicked.connect(lambda: self.submit())
         self.receipt_button.clicked.connect(lambda: self.receipt_print())
-
 
     def clear(self):
         self.exception_label.clear()
@@ -25,36 +25,33 @@ class Logic(QMainWindow, Ui_ProduceShop):
         self.dollars_label.clear()
 
     def convert_to_zero(self, text):
+        if text == '':
+            return 0.0
+        else:
+            return text
+
+    def submit(self):
         try:
-            if text == '':
-                return 0.0
-            else:
-                return float(text)
-        except ValueError:
-            self.exception_label.setText('Enter values that are numeric,\n e.g. 4 or 5.25. Input only numerical\n values; do not include "lbs"')
+            pear_cost = float(self.convert_to_zero(self.pear_input.text().strip()))
+            strawberries_cost = float(self.convert_to_zero(self.strawberry_input.text().strip()))
+
+            total = pear_cost + strawberries_cost
+            self.total_label.setText('TOTAL')
+            self.dollars_label.setText(f'${total:.2f}')
+            self.exception_label.clear()
+        except:
+            self.exception_label.setText(
+                'Enter values that are numeric,\n e.g. 4 or 5.25. Input only numerical\n values; do not include "lbs"')
+            self.total_label.clear()
             self.pear_input.clear()
             self.strawberry_input.clear()
             self.pineapple_input.clear()
             self.apple_input.clear()
             self.banana_input.clear()
             self.watermelon_input.clear()
-            self.total_label.clear()
             self.produce_label.clear()
             self.cost_label.clear()
             self.dollars_label.clear()
-            return 0
-
-    def submit(self):
-        pear_text = self.pear_input.text().strip()
-        pear_cost = self.convert_to_zero(pear_text)
-        strawberries_text = self.strawberry_input.text().strip()
-        strawberries_cost = self.convert_to_zero(strawberries_text)
-
-
-
-        total = pear_cost + strawberries_cost
-        self.total_label.setText('TOTAL')
-        self.dollars_label.setText(f'${total:.2f}')
 
     def receipt_print(self):
         self.exception_label.clear()
@@ -68,9 +65,9 @@ class Logic(QMainWindow, Ui_ProduceShop):
         self.produce_label.clear()
         self.cost_label.clear()
         self.dollars_label.clear()
-        file = 'recipt'
+        file = 'receipt'
         now = date.today()
-        with open(file, 'w') as recipt:
-            recipt.write('-'*50)
-            recipt.write('\nThe Produce Shop\n')
-            recipt.write(f'Omaha, Nebraska | Date: {now} ')
+        with open(file, 'w') as receipt:
+            receipt.write('-' * 50)
+            receipt.write('\nThe Produce Shop\n')
+            receipt.write(f'Omaha, Nebraska | Date: {now} ')
